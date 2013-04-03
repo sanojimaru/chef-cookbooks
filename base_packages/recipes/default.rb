e2dbb40d698fce1af28e1a9d410223cdc1954271
Hirohide Sano @sanojimaru <sanojimaru@gmail.com>
2013-04-03 20:05:45 +0900

    remove mosh from base_packages.

--- base_packages/recipes/default.rb
+++ base_packages/recipes/default.rb
@@ -6,8 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+execute "update yum packages" do
+  command "yum -y update"
+  ignore_failure true
+end
 
-%w{mosh sysstat gcc-c++ patch readline-devel zlib-devel libyaml-devel
+%w{sysstat gcc-c++ patch readline-devel zlib-devel libyaml-devel
   openssl-devel ImageMagick-devel yum-cron yum-fastestmirror
   libxml2-devel libxslt-devel pcre-devel}.each do |pkg|
   package pkg do
